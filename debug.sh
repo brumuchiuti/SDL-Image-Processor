1e79794c8c93e3308d6384915fdf37f2c61016b6
Lsarai23 <sarai.gg.lucas@gmail.com>
2026-03-26 22:20:49 -0300

    refactor: remove unused CMakeLists.txt, debug.sh, and run.sh files

--- debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Flag -j is used to enable parallel building, which can significantly speed up the build process on multi-core systems.
-cmake --build build -j
-cd build/Debug
-./main $1
