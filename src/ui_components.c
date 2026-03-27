7f7ea17c4f6113c0074f6098ce1239e7402b362f
Jessica Bispo <jessicabispo.work@gmail.com>
2026-03-26 22:52:30 -0300

    docs: add helper comments for UI collision detection function

--- src/ui_components.c
+++ src/ui_components.c
@@ -1,5 +1,7 @@
 #include "ui_components.h"
 
+// Helper function for mouse event detection in UI components
+// Checks if a point (x, y) is within the bounds of a rectangle
 bool is_point_inside_rectangle(float x, float y, SDL_FRect rectangle) {
     return (x >= rectangle.x && 
             x <= (rectangle.x + rectangle.w) &&
